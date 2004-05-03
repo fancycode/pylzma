6783d8ca934c80b486ea601bb786d0236313f5b8
Joachim Bauch <mail@joachim-bauch.de>
2004-05-03 20:29:22 +0000

    added keyword

--- tests/testall.py
+++ tests/testall.py
@@ -19,6 +19,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+# $Id$
+#
 import md5, random
 import pylzma
 import unittest
